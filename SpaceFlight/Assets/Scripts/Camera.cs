e441ec757ac837533db47744869938cc0d50a590
Cyprian <cyprianwalas1223@gmail.com>
2022-01-09 22:43:10 +0100

    Plants and GroundScen

--- SpaceFlight/Assets/Scripts/Camera.cs
+++ SpaceFlight/Assets/Scripts/Camera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
    public  Transform target;
-    Vector3 offset;
+   public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
